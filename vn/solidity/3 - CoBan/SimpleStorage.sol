6c80264525d08b5564e563ea729272e6507c4da4
ZxStim <thang.victor.luong@gmail.com>
2024-02-27 13:39:26 +0700

    added SimpleStorage contract

---
+++ vn/solidity/3 - CoBan/SimpleStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract SimpleStorage {
+
+    string public maintainerName = "zxstim";
+    uint8 public version = 1;
+    address public donationAddress = 0xe3d25540BA6CED36a0ED5ce899b99B5963f43d3F;
+
+    struct Person {
+        string name;
+        uint8 age;
+        bool overEighteen;
+        address uuid;
+        uint256 assetValue;
+        int256 debtValue;
+    }
+
+    Person[] private listOfPeople;
+
+    mapping(address => Person) uuidToPerson;
+
+    function storePerson(string memory _name, uint8 _age, bool _overEighteen, uint256 _assetValue, int256 _debtValue) public {
+        _assetValue *= 1e18;
+        _debtValue *= 1e18;
+        listOfPeople.push(Person({name: _name, age: _age, overEighteen: _overEighteen, uuid: msg.sender, assetValue: _assetValue, debtValue: _debtValue}));
+        uuidToPerson[msg.sender] = Person({name: _name, age: _age, overEighteen: _overEighteen, uuid: msg.sender, assetValue: _assetValue, debtValue: _debtValue});
+    }
+
+    function retrievePerson(address _address) public view returns (Person memory person) {
+        return uuidToPerson[_address];
+    }
+}
+
---
+++ vn/solidity/4 - NangCao/4 - Units.md
@@ -0,0 +1,10 @@
+# Đơn vị
+
+## Những đơn vị thường sử dụng
+- `wei` đơn vị nhỏ nhất (1 `wei` == 1)
+- `gwei` đơn vị thường được sử dụng để diễn tả giá gas hiện tại (1 `gwei` == 1e9)
+- `ether` đơn vị (1 `ether` == 1e18)
+
+## Keyword
+`this`
+`super`
