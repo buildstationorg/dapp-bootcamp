a8074fcb969d6c034fbf5c6c21abce23043b2126
ZxStim <thang.victor.luong@gmail.com>
2024-03-01 20:44:29 +0700

    added more info for NangCao

--- vn/README.md
+++ vn/README.md
@@ -6,7 +6,10 @@
 ​DApp Bootcamp là một chương trình đào tạo đặc biệt được thiết kế dành riêng cho các lập trình viên muốn mở rộng kiến thức và kỹ năng của mình trong lĩnh vực phát triển ứng dụng Web3. Qua chương trình này, bạn sẽ được trải nghiệm một lộ trình học tập bài bản, từ cơ bản đến nâng cao, về công nghệ blockchain, smart contracts, và cách thức xây dựng DApp hiệu quả. Đặc biệt, Bootcamp còn cung cấp cơ hội thực hành trực tiếp trên các dự án thực tế, giúp học viên không chỉ nắm vững kiến thức lý thuyết mà còn rèn luyện kỹ năng áp dụng vào thực tiễn, mở ra nhiều cơ hội nghề nghiệp trong tương lai.
 
 ## Giảng viên
-[@zxstim](https://github.com/zxstim)
+- [@zxstim](https://github.com/zxstim)
+
+## Hỗ trợ & đóng góp
+[Cộng đồng OpenForBuilders](https://www.openforbuilders.com)
 
 ## Tài trợ
 DApp Bootcamp này được tài trợ bởi ban tổ chức [Road to Devcon](https://blog.ethereum.org/2023/06/29/road-to-devcon7-grants).
--- vn/solidity/3 - CoBan/SimpleStorage.sol
+++ vn/solidity/3 - CoBan/SimpleStorage.sol
@@ -4,30 +4,38 @@ pragma solidity ^0.8.20;
 
 contract SimpleStorage {
 
+    // Khai báo các biến liên quan đến người sáng lập
     string public maintainerName = "zxstim";
+    // Khai báo phiên bản của contract
     uint8 public version = 1;
+    // Địa chỉ nhận quyên góp
     address public donationAddress = 0xe3d25540BA6CED36a0ED5ce899b99B5963f43d3F;
 
+    // Khai báo cấu trúc Person
     struct Person {
-        string name;
-        uint8 age;
-        bool overEighteen;
-        address uuid;
-        uint256 assetValue;
-        int256 debtValue;
+        string name; // Tên
+        uint8 age; // Tuổi
+        bool overEighteen; // Đủ 18 tuổi
+        address uuid; // UUID
+        uint256 assetValue; // Giá trị tài sản
+        int256 debtValue; // Giá trị nợ
     }
 
-    Person[] private listOfPeople;
+    Person[] private listOfPeople; // syntax này có nghĩa là tạo một Array chứa các Person tên là listOfPeople
+    
+    mapping(address => Person) uuidToPerson; // syntax này có nghĩa là tạo một mapping từ address sang Person tên là uuidToPerson
 
-    mapping(address => Person) uuidToPerson;
-
+    // function lưu thông tin của 1 person mới với các thông tin như name, age, overEighteen, assetValue, debtValue
     function storePerson(string memory _name, uint8 _age, bool _overEighteen, uint256 _assetValue, int256 _debtValue) public {
-        _assetValue *= 1e18;
-        _debtValue *= 1e18;
+        _assetValue *= 1e18; // Chuyển đổi giá trị tài sản sang đơn vị wei
+        _debtValue *= 1e18; // Chuyển đổi giá trị nợ sang đơn vị wei
+        // Thêm thông tin của person mới vào danh sách listOfPeople
         listOfPeople.push(Person({name: _name, age: _age, overEighteen: _overEighteen, uuid: msg.sender, assetValue: _assetValue, debtValue: _debtValue}));
+        // Thêm thông tin của person mới vào mapping uuidToPerson
         uuidToPerson[msg.sender] = Person({name: _name, age: _age, overEighteen: _overEighteen, uuid: msg.sender, assetValue: _assetValue, debtValue: _debtValue});
     }
 
+    // function lấy thông tin của 1 person dựa trên địa chỉ
     function retrievePerson(address _address) public view returns (Person memory person) {
         return uuidToPerson[_address];
     }
